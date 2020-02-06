6944ce76423768a7e0b4678c977bffecc9cabdfb
Craig-Benson <craigbenson@outlook.ie>
2020-02-06 09:24:51 +0000

    copied FastCollinearPoints class from the assignment

---
+++ src/com/company/FastCollinearPoints.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {}    // finds all line segments containing 4 or more points
+    public           int numberOfSegments()    {
+        return 0;
+    }    // the number of line segments
+    public LineSegment[] segments()   {
+        return new LineSegment[0];
+    }             // the line segments
+}
