f9a1761856fbf5db5057935cf9a160195ac29186
Craig-Benson <craigbenson@outlook.ie>
2020-02-06 09:22:00 +0000

    copied BruteColinniearPoints class from the assignment

---
+++ src/com/company/BruteCollinearPoints.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points)  {}  // finds all line segments containing 4 points
+    public int numberOfSegments()  {return 0;}       // the number of line segments
+    public LineSegment[] segments()  {
+        return new LineSegment[0];
+    }               // the line segments
+}
